1d72ecce85f6c75e027a48d4e9874de93d70eba3
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-04-21 16:58:31 -0500

    adding some jQuery

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,37 @@
 // Game.js psuedo coding
 
 // Declare Varibles
+var randomNum = $('#randNum');
+var totalCount = []; //TODO Math Array
+    
+randomNum = Math.floor(Math.random() * 100) + 1;
+
+console.log('randNum is ' + randomNum)
+
+$('#randNum').append(randomNum);
+    
+
 
 // for loop for win/ lose counter
 
-//random number generator for win con
+//random number generator gems
 
-// event handlers
-    //random number generator for gems
+// *********************** RNG For the Gems ************************ // 
+var gemOne = $('#one');
+var gemTwo = $('#two');
+var gemThree = $('#three');
+var gemFour = $('#four');
+
+    gemOne = Math.floor(Math.random() * 10 + 1)
+    gemTwo = Math.floor(Math.random() * 10 + 1)
+    gemThree = Math.floor(Math.random() * 10 + 1)
+    gemFour = Math.floor(Math.random() * 10 + 1)
+
+    console.log('Gem One is ' + gemOne);
+    console.log('Gem Two is ' + gemTwo);
+    console.log('Gem Three is ' + gemThree);
+    console.log('Gem Four is ' + gemFour);
+
 
 //if user score is equal to computer score 
     //user recives a win
