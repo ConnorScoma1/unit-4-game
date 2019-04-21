6211f66d9ed91263a6e3ad5336ec6bb336d34363
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-04-21 10:34:42 -0500

    js psuedo code

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+// Game.js psuedo coding
+
+// Declare Varibles
+
+// for loop for win/ lose counter
+
+//random number generator for win con
+
+// event handlers
+    //random number generator for gems
+
+//if user score is equal to computer score 
+    //user recives a win
+
+    //else if user score is > computer score
+        //user will recive a loss
+    
